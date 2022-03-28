406404827f18444bea04cdad4d585021a570b231
C. Thomas Wyrth <mmxvi.ctw@gmail.com>
2022-03-28 10:45:42 -0700

    3rd commit, messages sending but not propogating

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,56 +1,52 @@
 import { useState, useEffect } from 'react';
-import { io } from "socket.io-client";
+import io from "socket.io-client";
 import { useParams } from 'react-router-dom';
 
+const socket = io.connect('http://localhost:4000');
 
 export default props => {
-   const [ socket ] = useState(() => io(':4000'));
-
    const { chatName } = useParams();
    const [ state, setState ] = useState({message: '', name: chatName});
    const [ chat, setChat ] = useState([]);
 
+   const onMessageSubmit = async () => {
+      if (state.message !== "") {
+         const {name, message} = state;
+         await socket.emit('send_message', {name, message});
+         setChat((chat) => [...chat, {name, message}]);
+         setState({name, message: ''});
+      }
+   }
+   
    useEffect(() => {
-      socket.on('message', ({name, message}) => {
-         setChat([...chat, {name, message}]);
-         console.log(chat)
+      socket.on('receive_message', ({name, message}) => {
+         setChat((chat) => [...chat, {name, message}]);
       });
-      return () => socket.disconnect(true);
-   }, [chat]);
+   }, [socket]);
 
    const onTextChange = (e) => {
       setState({...state, [e.target.name]: e.target.value})
       console.log("onTextChange firing:", state);
    }
 
-   const onMessageSubmit = (e) => {
-      const {name, message} = state;
-      socket.emit('message', {name, message});
-      e.preventDefault();
-      setState({message: '', name});
-      console.log(state);
-      console.log(chat);
-   }
-
-   const renderChat = () => {
-      return chat.map(({name, message}, idx) => (
-         <div key={idx} className="ps-2">
-            <p><strong>{name}:</strong> <span>{message}</span></p>
-         </div>
-      ))
-   }
 
    return (
       <div className="container mt-4">
          <form className="col-6 mx-auto" onSubmit={onMessageSubmit}>
             <div className="input-group mb-3">
-               <input type="text" name="message" className="form-control" onChange={e => onTextChange(e)} value={state.message} />
+               <input type="text" name="message" className="form-control" onChange={e => onTextChange(e)} onKeyPress={(e) => {e.key === "Enter" && onMessageSubmit()}} value={state.message} />
                <button className="btn btn-secondary">Send</button>
             </div>
          </form>
          <div className="col-6 mx-auto border border-1 border-info rounded p-2">
             <h4>Chat Log:</h4>
-            { renderChat() }
+            { chat.map(({name, message}, idx) => {
+               return (
+                  <div key={idx} className={chatName === name ? 'text-start ps-2' : 'text-end pe-2'}>
+                     <p><strong>{name}:</strong> <span>{message}</span></p>
+                  </div>
+               )}
+            )}
          </div>
       </div>
    );
--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+app.use(cors());
+
 const server = http.createServer(app);
 
-const io = require('socket.io')(server, { cors: true });
+const io = new Server(server, {
+   cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST']
+   },
+});
 
 io.on("connection", (socket) => {
-   socket.on('message', (data) => {
-      io.emit('message', data);
-      console.log(data);
+   console.log('user connected:', socket.id);
+
+   socket.on('send_message', (data) => {
+      socket.emit('receive_message', data);
+   });
+
+   socket.on('disconnect', () => {
+      console.log('user disconnected:', socket.id);
    });
 });
 
